770e47e5711bb04c5dd043eaa481789f8dfce338
Gilgamesh555 <painarniano@gmail.com>
2023-09-30 14:44:20 -0400

    problems A + B + C

---
+++ Gym_Contests/The Andover Computing Open (TACO) 2023/B.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define MOD 1000000007
+#define MAX 200002
+#define MODT 998244353
+using namespace std;
+typedef long long int ll;
+
+// __builtin_popcount -> counts the number of 1's in a posit number
+// __lg -> returns the most largest bit 1's in a number
+
+const int MaxBound = 1e5 * 2 + 2;
+const ll inf = 1e17;
+const string PI = "3141592653589793238462643383279";
+ 
+void solve()
+{
+    
+}
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+
+    // int t;
+    // cin>>t;
+    // while (t--) {
+    //     solve();
+    // }
+    
+    return 0;
+}
